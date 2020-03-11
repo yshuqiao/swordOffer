cf34fe04a265270e272424c08afcb7a6412f801b
yshuqiao <yshuqiao@qq.com>
2020-03-11 11:26:37 +0800

    '20200311'

--- src/duplicateFigures.java
+++ src/duplicateFigures.java
@@ -1,5 +1,5 @@
 /*
-在一个长度为 n 的数组里的所有数字都在 0 到 n-1 的范围内。数组中某些数字是重复的，
+题1:在一个长度为 n 的数组里的所有数字都在 0 到 n-1 的范围内。数组中某些数字是重复的，
 但不知道有几个数字是重复的，也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
 要求时间复杂度 O(N)，空间复杂度 O(1)。因此不能使用排序的方法，也不能使用额外的标记数组。
 对于这种数组元素在 [0, n-1] 范围内的问题，可以将值为 i 的元素调整到第 i 个位置上进行求解。
@@ -35,6 +35,6 @@ public class duplicateFigures {
         int [] duplication = new int[len] ;
         boolean b = duplicatfigures.duplicate(a,len,duplication);
         System.out.println(b);
-        System.out.println(duplication[0]);
+        System.out.println("找到任意一个重复数字："+duplication[0]);
     }
 }
